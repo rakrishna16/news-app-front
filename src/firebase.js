40bd6b3c9a26c47655cb2d4369b922534db06057
rakrishna16 <rakrishna16@gmail.com>
2025-02-19 09:35:06 +0530

    string

--- src/firebase.js
+++ src/firebase.js
@@ -12,12 +12,12 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_ADMIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: String(import.meta.env.VITE_API_KEY),
+  authDomain: String(import.meta.env.VITE_AUTH_ADMIN),
+  projectId: String(import.meta.env.VITE_PROJECT_ID),
+  storageBucket: String(import.meta.env.VITE_STORAGE_BUCKET),
+  messagingSenderId: String(import.meta.env.VITE_MESSAGE_SENDER_ID),
+  appId: String(import.meta.env.VITE_APP_ID),
 };
 
 // Initialize Firebase
